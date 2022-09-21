6c65ced108b79646d1264f8d94b7abf44e9e13c4
Joshua Hunsche Jones <jhunschejones@github.com>
2022-09-21 12:31:23 -0500

    Implement a simple dictionary search

--- eiwa_test/main.rb
+++ eiwa_test/main.rb
@@ -1,11 +1,17 @@
 require "eiwa"
+require "pry"
 
-limit = 10
-counter = 0
+search = "おはようございます"
+found_word = nil
+
+# https://github.com/searls/eiwa#passing-a-block
 Eiwa.parse_file("jmdict.xml", type: :jmdict_e) do |entry|
-  puts entry.inspect
 
-  counter += 1
-
-  break if counter == limit
+  if entry&.readings&.first&.text == search || entry&.spellings&.first&.text == search
+    found_word = entry
+    break
+  end
 end
+
+# found_word
+binding.pry
