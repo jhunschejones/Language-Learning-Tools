e4bdca121480193dc6f2b79794429376fc15c064
Joshua Hunsche Jones <jhunschejones@github.com>
2022-09-21 12:34:47 -0500

    Implement a simple kanji search

--- eiwa_test/main.rb
+++ eiwa_test/main.rb
@@ -1,17 +1,25 @@
 require "eiwa"
 require "pry"
 
-search = "おはようございます"
+word_search = "おはようございます"
 found_word = nil
-
 # https://github.com/searls/eiwa#passing-a-block
 Eiwa.parse_file("jmdict.xml", type: :jmdict_e) do |entry|
-
-  if entry&.readings&.first&.text == search || entry&.spellings&.first&.text == search
+  if entry&.readings&.first&.text == word_search || entry&.spellings&.first&.text == word_search
     found_word = entry
     break
   end
 end
 
-# found_word
+kanji_search = "私"
+found_kanji = nil
+# https://github.com/searls/eiwa#passing-a-block
+Eiwa.parse_file("kanjidic2.xml", type: :kanjidic2) do |entry|
+  if entry&.text == kanji_search
+    found_kanji = entry
+    break
+  end
+end
+
+# Look at `found_word` or `found_kanji`
 binding.pry
