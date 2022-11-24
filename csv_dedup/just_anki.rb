77cdd25fbcd87b0817386e3cbf38bd8123d715b7
jhunschejones <joshjones103@gmail.com>
2022-11-23 18:50:03 -0600

    Account for duplicate dates in multitimer.csv

--- csv_dedup/just_anki.rb
+++ csv_dedup/just_anki.rb
@@ -33,18 +33,20 @@ CSV.open("just_anki/japanese_study_log.csv", "w") do |csv|
   anki_rows.reverse.each { |row| csv << row }
 end
 
-
-anki_rows = []
+# multitimer can have more than one entry per date so this also combines the entries
+anki_rows = Hash.new(0)
 CSV.foreach("multitimer.csv") do |row|
   if row.first == "Study Flashcards"
     # Name,Date,Seconds,Comment
     date = row[1][0..11] # "Nov 12, 2022 at 2:53:35 PM"
     time = row[2].to_f / 60 # This is in seconds in the CSV
-    anki_rows << [Date.strptime(date, "%b %d, %Y").strftime("%m/%d/%Y"), time]
+
+    date_string = Date.strptime(date, "%b %d, %Y").strftime("%m/%d/%Y")
+    anki_rows[date_string] += time
   end
 end
 
 CSV.open("just_anki/multitimer.csv", "w") do |csv|
   csv << ["Date", "Time (mins)"]
-  anki_rows.reverse.each { |row| csv << row }
+  anki_rows.to_a.reverse.each { |row| csv << row }
 end
